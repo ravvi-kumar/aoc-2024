581ae09558d3f50e15d5ddd101465fcf235a014c
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-12-08 20:25:28 +0530

    day6

--- day06/a.ts
+++ day06/a.ts
@@ -6,7 +6,6 @@ console.log(board);
 
 let position = [0, 0];
 
-// find starting point
 input.forEach((line, idx) => {
   if (line.includes("^")) {
     const point = line.indexOf("^");
@@ -16,66 +15,44 @@ input.forEach((line, idx) => {
 
 console.log(position, "STARTING POINT", board[position[0]][position[1]]);
 
-let direction = "UP";
-let moveX = 0;
-let moveY = -1;
-
-function getNextDirection() {
-  let nextDirection = direction;
-  if (direction === "UP") {
-    nextDirection = "RIGHT";
-  } else if (direction === "RIGHT") {
-    nextDirection = "DOWN";
-  } else if (direction === "DOWN") {
-    nextDirection = "LEFT";
-  } else if (direction === "LEFT") {
-    nextDirection = "UP";
-  }
-  return nextDirection;
+function getNextDirection(currentDirection: string) {
+  if (currentDirection === "UP") return "RIGHT";
+  if (currentDirection === "RIGHT") return "DOWN";
+  if (currentDirection === "DOWN") return "LEFT";
+  return "UP";
 }
 
-function turn() {
-  console.log("direction", direction);
-  const nextDirection = getNextDirection();
-
-  if (nextDirection === "UP") {
-    moveX = 0;
-    moveY = -1;
-  } else if (nextDirection === "DOWN") {
-    moveX = 0;
-    moveY = 1;
-  } else if (nextDirection === "LEFT") {
-    moveX = -1;
-    moveY = 0;
-  } else if (nextDirection === "RIGHT") {
-    moveX = 1;
-    moveY = 0;
-  }
-  direction = nextDirection;
-
-  console.log("turn", direction);
+function getMovement(direction: string): [number, number] {
+  if (direction === "UP") return [0, -1];
+  if (direction === "RIGHT") return [1, 0];
+  if (direction === "DOWN") return [0, 1];
+  return [-1, 0];
 }
 
-let nextPosition: number[] | undefined = position;
+let nextPosition: number[] = position;
 const moves: string[] = [`${position[0]},${position[1]}`];
+let direction = "UP";
 
 function move() {
+  const [moveX, moveY] = getMovement(direction);
   const y = position[0] + moveY;
   const x = position[1] + moveX;
   nextPosition = [y, x];
-  console.log("nextPosition", nextPosition);
+
   if (y < 0 || x < 0 || y >= board.length || x >= board[y].length) {
-    nextPosition = undefined;
+    return false
   } else if (board[y][x] === "#") {
-    turn();
+    direction = getNextDirection(direction);
   } else {
     position = nextPosition;
     moves.push(`${y},${x}`);
   }
+  return true
 }
 
-while (nextPosition !== undefined) {
-  move();
+let canContinue = true;
+while (canContinue) {
+  canContinue = move();
 }
 const moveCount = new Set(moves).size;
 
--- day06/b.ts
+++ day06/b.ts
@@ -1,82 +1,81 @@
-const input = Deno.readTextFileSync("./example2.txt").split("\n");
+const input = Deno.readTextFileSync("./input.txt").split("\n");
 console.log(input);
 
 const board = input.map((line) => line.split(""));
 console.log(board);
 
 let position = [0, 0];
+let starting = [0, 0];
 
 // find starting point
 input.forEach((line, idx) => {
   if (line.includes("^")) {
     const point = line.indexOf("^");
     position = [idx, point];
+    starting = [idx, point];
   }
 });
 
 console.log(position, "STARTING POINT", board[position[0]][position[1]]);
 
-let direction = "UP";
-let moveX = 0;
-let moveY = -1;
-
-function getNextDirection() {
-  let nextDirection = direction;
-  if (direction === "UP") {
-    nextDirection = "RIGHT";
-  } else if (direction === "RIGHT") {
-    nextDirection = "DOWN";
-  } else if (direction === "DOWN") {
-    nextDirection = "LEFT";
-  } else if (direction === "LEFT") {
-    nextDirection = "UP";
-  }
-  return nextDirection;
+function getNextDirection(currentDirection: string) {
+  if (currentDirection === "UP") return "RIGHT";
+  if (currentDirection === "RIGHT") return "DOWN";
+  if (currentDirection === "DOWN") return "LEFT";
+  return "UP";
 }
 
-function turn() {
-  console.log("direction", direction);
-  const nextDirection = getNextDirection();
-
-  if (nextDirection === "UP") {
-    moveX = 0;
-    moveY = -1;
-  } else if (nextDirection === "DOWN") {
-    moveX = 0;
-    moveY = 1;
-  } else if (nextDirection === "LEFT") {
-    moveX = -1;
-    moveY = 0;
-  } else if (nextDirection === "RIGHT") {
-    moveX = 1;
-    moveY = 0;
-  }
-  direction = nextDirection;
-
-  console.log("turn", direction);
+function getMovement(direction: string): [number, number] {
+  if (direction === "UP") return [0, -1];
+  if (direction === "RIGHT") return [1, 0];
+  if (direction === "DOWN") return [0, 1];
+  return [-1, 0]; // LEFT
 }
 
-let nextPosition: number[] | undefined = position;
-const moves: string[] = [`${position[0]},${position[1]}`];
+function checkLoop(startY: number, startX: number): boolean {
+  if (startY === starting[0] && startX === starting[1]) return false;
 
-function move() {
-  const y = position[0] + moveY;
-  const x = position[1] + moveX;
-  nextPosition = [y, x];
-  console.log("nextPosition", nextPosition);
-  if (y < 0 || x < 0 || y >= board.length || x >= board[y].length) {
-    nextPosition = undefined;
-  } else if (board[y][x] === "#") {
-    turn();
-  } else {
-    position = nextPosition;
-    moves.push(`${y},${x}`);
+  const visited = new Set<string>();
+  let pos = [...starting];
+  let dir = "UP";
+
+  while (true) {
+    const [moveX, moveY] = getMovement(dir);
+    const nextY = pos[0] + moveY;
+    const nextX = pos[1] + moveX;
+
+    if (
+      nextY < 0 ||
+      nextX < 0 ||
+      nextY >= board.length ||
+      nextX >= board[0].length
+    ) {
+      return false;
+    }
+
+    if (board[nextY][nextX] === "#" || (nextY === startY && nextX === startX)) {
+      const state = `${pos[0]},${pos[1]},${dir}`;
+      if (visited.has(state)) {
+        return true;
+      }
+      visited.add(state);
+      dir = getNextDirection(dir);
+    } else {
+      pos = [nextY, nextX];
+    }
   }
 }
 
-while (nextPosition !== undefined) {
-  move();
-}
-const moveCount = new Set(moves).size;
+let loopCount = 0;
 
-console.log(moveCount, "MOVES");
+// Test each position on the board
+for (let i = 0; i < board.length; i++) {
+  for (let j = 0; j < board[i].length; j++) {
+    if (board[i][j] === "#") continue;
+    if (checkLoop(i, j)) {
+      loopCount++;
+    }
+  }
+}
+
+console.log(loopCount, "TOTAL");
